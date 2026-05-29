e4b6b09af33efb289c6a198d210d42332887ab76
Emmadev <emmanuebustosdev@gmail.com>
2026-05-29 11:55:45 -0300

    Ultimos ajustes del sistema para la entrega 3

--- supabase_profiles_expansion.sql
+++
@@ -1,4 +0,0 @@
--- Script para añadir columnas de redes sociales a la tabla profiles
-ALTER TABLE public.profiles 
-ADD COLUMN IF NOT EXISTS github_user text,
-ADD COLUMN IF NOT EXISTS linkedin_user text;
